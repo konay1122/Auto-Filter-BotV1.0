86e955ebbf3eff568bdbccb3aa6b84f442e3c03f
Mafia58 <90953455+Mafia58@users.noreply.github.com>
2021-11-06 07:57:21 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,41 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+හායි මම තමයි  Auto Filter Bot !! :D
+This is Version 1.0 of Auto Filter Bot
+මට පුලුවන් ඔයාගෙ එකේ තියෙන files වල button link අරන් දෙන්න!
+For more click <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>කොහොමද මාව පාවිච්චි කරන්නෙ???</b>
+<i>
+* මුලින්ම මාව ඔයාගෙ group එකේ admin කෙනෙක් කරන්න.
+* මාව ඔයාගෙ channels එකෙත් admin කෙනෙක් කරන්න.<b>admin rights ඔක්කොමත් එක්කම.</b>!
+</i>
+*ඉතුරු ටික ඉංග්‍රීසියෙන්ම කියන්නම්😁
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+* <code>/add channelid</code>  -  Links channel to your group.
+or
+* <code>/add @channelusername</code> - Links channel to your group.
+<i>NOTE : You can get your channel ID from @Get_Channel_User_Telegram_ID_Bot </i>
+* <code>/del channelid</code>  -  Delinks channel from group
+or
+* <code>/del @channelusername</code>  -  Delinks channel from group
+<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+* <code>/delall</code>  -  Removes all connected channels and filters from group!
+<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
+* <code>/filterstats</code>  -  Check connected channels and number of filters.
+No need add each filter again!
+Bot will automatically search for your files and give links to that!
+<b>© @TroJanzHEX</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V1</b>
+    
+⭕️<b>Creater :</b> @akalankanime
+⭕️<b>Language :</b> <code>Python3.10</code>
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Tutorial Video :</b> <a href=''>පස්සෙ දාන්නම්</a> 
+"""
