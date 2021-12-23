dcfe42eee407fef0558b9287eeb4091dd98e4dbd
N.A.S <88653689+konay1122@users.noreply.github.com>
2021-12-24 06:22:45 +0630

    Update script.py

--- script.py
+++ script.py
@@ -2,40 +2,68 @@ class script(object):
 
 
     START_MSG = """ <b>Hi {}
-හායි මම තමයි  Auto Filter Bot !! :D
-This is Version 1.0 of Auto Filter Bot
-මට පුලුවන් ඔයාගෙ එකේ තියෙන files වල button link අරන් දෙන්න!
+
+You can call this as an Auto Filter Bot if you like :D
+
+This is Version 2 of Auto Filter Bot
+
+Bot gives button link to files in connected channels on query !
+
+No need to add filters for your files or movies from now on!
+
 For more click <i>help</i></b>"""
 
 
-    HELP_MSG = """<b>කොහොමද මාව පාවිච්චි කරන්නෙ???</b>
+    HELP_MSG = """<b>How to use the bot??</b>
+
 <i>
-* මුලින්ම මාව ඔයාගෙ group එකේ admin කෙනෙක් කරන්න.
-* මාව ඔයාගෙ channels එකෙත් admin කෙනෙක් කරන්න.<b>admin rights ඔක්කොමත් එක්කම.</b>!
+* Add bot to your group with admin rights.
+
+* Add bot to channels which you want to link with <b>all admin rights</b>!
 </i>
-*ඉතුරු ටික ඉංග්‍රීසියෙන්ම කියන්නම්😁
+
+
 <b>Bot Commands - Works in Group only</b>
 (You need to be a Auth User in order to use these commands)
+
 * <code>/add channelid</code>  -  Links channel to your group.
 or
 * <code>/add @channelusername</code> - Links channel to your group.
-<i>NOTE : You can get your channel ID from @Get_Channel_User_Telegram_ID_Bot </i>
+
+<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+
+
 * <code>/del channelid</code>  -  Delinks channel from group
 or
 * <code>/del @channelusername</code>  -  Delinks channel from group
+
 <i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+
+
 * <code>/delall</code>  -  Removes all connected channels and filters from group!
+
 <i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
+
+
 * <code>/filterstats</code>  -  Check connected channels and number of filters.
+
+
+
 No need add each filter again!
 Bot will automatically search for your files and give links to that!
+
+
 <b>© @TroJanzHEX</b>"""
 
 
-    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V1</b>
+    ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
     
-⭕️<b>Creater :</b> @akalankanime
-⭕️<b>Language :</b> <code>Python3.10</code>
+⭕️<b>Creater :</b> @TroJanzHEX
+
+⭕️<b>Language :</b> <code>Python3</code>
+
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-⭕️<b>Tutorial Video :</b> <a href=''>පස්සෙ දාන්නම්</a> 
+
+⭕️<b>Tutorial Video :</b> <a href='https://youtu.be/KQVYQAOsFYY'>Video Link</a> 
+
 """
